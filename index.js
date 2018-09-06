b2ee84f51ef1db27fe311c880c254548ae3d0ed1
nursh <nsheikhmohammed@yahoo.com>
2018-09-06 15:01:36 -0400

    Finished wiring Google authentication for project

--- index.js
+++ index.js
@@ -6,17 +6,26 @@ const PORT = process.env.PORT || 5000;
 
 
 const app = express();
+
 const googleCredentials = {
   clientID: process.env.googleClientID,
   clientSecret: process.env.googleClientSecret,
   callbackURL: '/auth/google/callback',
 }
-const googleCallback = (accessToken) => {
-  console.log(accessToken)
+const googleCallback = (accessToken, refreshToken, profile, done) => {
+  console.log("accessToken: ", accessToken);
+  console.log("refreshToken: ", refreshToken);
+  console.log("profile: ", profile);
 }
 passport.use(new GoogleStrategy(googleCredentials, googleCallback));
 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
 
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT);
-
