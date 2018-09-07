b7e724c8f620f0cae6a7c82d573fa3381b8b2341
nursh <nsheikhmohammed@yahoo.com>
2018-09-07 08:13:52 -0400

    Initialized mongoose and setup configuration, created User model for saving users upon sign-up

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport'); // we are not exporting anything, just need execution
+require('./models/User');
+
 
 const PORT = process.env.PORT || 5000;  
 const app = express();
+
+mongoose.connect(process.env.mongoURI);
+
 require('./routes/authRoutes')(app); // getting function export that requires app as argument
 
 
