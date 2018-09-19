c274c596502e76e09c8a3ae6ac5c4836215cb427
nursh <nsheikhmohammed@yahoo.com>
2018-09-19 16:09:24 -0400

    Created Landing Component with centered text

---
+++ client/src/components/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Landing = () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>Emaily App</h1>
+    Collect feedback from Users
+  </div>
+);
+
+export default Landing;
