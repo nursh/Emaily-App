ef7632e59090fd95f98d2d58c6af8d1eebcf7bc7
nursh <nsheikhmohammed@yahoo.com>
2018-09-27 13:10:58 -0400

    Added Survey model to api/surveys route to deal with creating new Survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,19 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const requireCredit = require('../middlewares/requireCredit');
+
+const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-  app.post('/api/surveys', requireLogin, (req, res) => {
-
+  app.post('/api/surveys', requireLogin, requireCredit, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
   });
 };
