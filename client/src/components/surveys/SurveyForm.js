20619f28e6423cd02afa2e308a3edf3610b720ce
nursh <nsheikhmohammed@yahoo.com>
2018-10-01 15:36:33 -0400

    Added SurveyField to SurveyForm to handle each field

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { reduxForm, Field} from 'redux-form';
+
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+
+  renderField = () => {
+    return (
+      <div>
+        <Field type="text" name="surveyTitle" component={SurveyField} />
+      </div>
+    );
+  }
   render() {
     return (
       <div>
         SurveyForm
+        <form
+          onSubmit={this.props.handleSubmit((values) => console.log(values))}
+        >
+          {this.renderField()}
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
 }
 
-export default SurveyForm;
+export default reduxForm({
+  form: 'surveyForm',
+})(SurveyForm);
