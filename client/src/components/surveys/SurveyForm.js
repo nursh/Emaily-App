9a5a1a14f68730005a74eec0d024a55eeb1a1129
nursh <nsheikhmohammed@yahoo.com>
2018-10-01 16:23:17 -0400

    Added all the fields for creating a new survey

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -8,7 +8,30 @@ class SurveyForm extends Component {
   renderField = () => {
     return (
       <div>
-        <Field type="text" name="surveyTitle" component={SurveyField} />
+        <Field
+          type="text"
+          label="Survey Title"
+          name="title"
+          component={SurveyField}
+        />
+        <Field
+          type="text"
+          label="Subject Line"
+          name="subject"
+          component={SurveyField}
+        />
+        <Field
+          type="text"
+          label="Email Body"
+          name="body"
+          component={SurveyField}
+        />
+        <Field
+          type="text"
+          label="Recipient List"
+          name="emails"
+          component={SurveyField}
+        />
       </div>
     );
   }
