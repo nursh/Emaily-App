f743cb2ab5348f0db9d284fab53434cc6d1b8a54
nursh <nsheikhmohammed@yahoo.com>
2018-10-01 20:47:14 -0400

    Rendering all the values gotten from the SurveyForm in SurveyReview

--- client/src/components/surveys/SurveyReview.js
+++ client/src/components/surveys/SurveyReview.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 
 const SurveyReview = ({ onCancel, formValues }) => {
+
+  const reviewFields = () => {
+    return formFields.map(({ label, name }) => (
+      <div key={name}>
+        <label>{label}</label>
+        <div>
+          {formValues[name]}
+        </div>
+      </div>
+    ))
+  }
+
   return (
     <div>
       <h5>Please review your entries before submission</h5>
       <div>
-        <div>
-          <label>Survey Title</label>
-          <div>{formValues.title}</div>
-        </div>
+        {reviewFields()}
       </div>
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
